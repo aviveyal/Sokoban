9ec2e27a78cb2337b34633d8e3099eb392a1f010
Aviv Eyal <204518997@stud.colman.ac.il>
2017-05-05 20:34:19 +0300

    SearchLib

--- SokobanProject/src/SerachLib/BFS.java
+++ SokobanProject/src/SerachLib/BFS.java
@@ -1,11 +1,5 @@
 package SerachLib;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-
-
 public class BFS<T> extends CommonSearcher<T> {
 
 	@Override

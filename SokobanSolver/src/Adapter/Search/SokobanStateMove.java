528dea08ae119baf0c3aaedefb96f930541d61f8
Aviv Eyal <204518997@stud.colman.ac.il>
2017-06-10 23:21:05 +0300

    made jar files

--- SokobanSolver/src/Adapter/Search/SokobanStateMove.java
+++ SokobanSolver/src/Adapter/Search/SokobanStateMove.java
@@ -7,9 +7,9 @@ public class SokobanStateMove {
 	private Position player;
 	private char[][] leveldata;
 
-	public SokobanStateMove(Position player ,char [][] leveldata) {
+	public SokobanStateMove(Position player, char[][] leveldata) {
 		this.player = player;
-		this.leveldata =leveldata;
+		this.leveldata = leveldata;
 	}
 
 	public char[][] getLeveldata() {
@@ -30,26 +30,21 @@ public class SokobanStateMove {
 
 	@Override
 	public String toString() {
-		
+
 		String str = "";
-		
-		for(int i=0 ; i<this.leveldata.length;i++)
-			for(int j=0;j<this.leveldata[i].length;j++)
-			{
-				if(j==this.leveldata[i].length-1)
-				{
-					str+="\n";
-				}
-				else
-				str+=this.leveldata[i][j];
+
+		for (int i = 0; i < this.leveldata.length; i++)
+			for (int j = 0; j < this.leveldata[i].length; j++) {
+				if (j == this.leveldata[i].length - 1) {
+					str += "\n";
+				} else
+					str += this.leveldata[i][j];
 			}
-		
-		
-		/*str += this.player.getRow();
-		str+=",";
-		str += this.player.getCol();
-		str += "\n";
-		*/
+
+		/*
+		 * str += this.player.getRow(); str+=","; str += this.player.getCol();
+		 * str += "\n";
+		 */
 		return str;
 	}
 
@@ -61,23 +56,19 @@ public class SokobanStateMove {
 	@Override
 	public boolean equals(Object obj) {
 		SokobanStateMove state = (SokobanStateMove) obj;
-		
-		for(int i=0 ; i<this.leveldata.length;i++)
-			for(int j=0;j<this.leveldata[i].length;j++)
-			{
-				if(this.leveldata[i][j]!= state.leveldata[i][j] )
-				{
+
+		for (int i = 0; i < this.leveldata.length; i++)
+			for (int j = 0; j < this.leveldata[i].length; j++) {
+				if (this.leveldata[i][j] != state.leveldata[i][j]) {
 					return false;
-					
+
 				}
 			}
 		/*
-		if (this.getPlayer().getRow() != state.getPlayer().getRow()
-				|| this.getPlayer().getCol() != state.getPlayer().getCol())
-			return false;
-		else
-			return true;
-			*/
+		 * if (this.getPlayer().getRow() != state.getPlayer().getRow() ||
+		 * this.getPlayer().getCol() != state.getPlayer().getCol()) return
+		 * false; else return true;
+		 */
 		return true;
 	}
 }
